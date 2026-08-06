170fd15b3b082284603fc094e2c58295220db3ee
Folau1 <138153709+Folau1@users.noreply.github.com>
2026-08-06 15:06:40 +0500

    Поставил финальный main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,28 @@ terraform {
     docker = {
       source = "kreuzwerker/docker"
     }
+
+    random = {
+      source = "hashicorp/random"
+    }
   }
-  required_version = "~>1.12.0" /*Многострочный комментарий.
- Требуемая версия terraform */
+
+  required_version = "~>1.12.0"
 }
-provider "docker" {}
 
-#однострочный комментарий
+provider "docker" {
+  context = "yc-remote"
+}
 
-resource "random_password" "random_string" {
+resource "random_password" "mysql_root_password" {
+  length      = 20
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+resource "random_password" "mysql_user_password" {
   length      = 16
   special     = false
   min_upper   = 1
@@ -19,19 +32,26 @@ resource "random_password" "random_string" {
   min_numeric = 1
 }
 
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
+resource "docker_image" "mysql" {
+  name         = "mysql:8"
   keep_locally = true
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "hello_world"
+resource "docker_container" "mysql" {
+  name  = "mysql"
+  image = docker_image.mysql.image_id
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=${random_password.mysql_root_password.result}",
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_USER=wordpress",
+    "MYSQL_PASSWORD=${random_password.mysql_user_password.result}",
+    "MYSQL_ROOT_HOST=%"
+  ]
 
   ports {
-    internal = 80
-    external = 9090
+    internal = 3306
+    external = 3306
+    ip       = "127.0.0.1"
   }
 }
-
